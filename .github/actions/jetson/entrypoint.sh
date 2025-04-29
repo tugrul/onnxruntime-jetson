4193091e4ba5a2c2a1fc219a0649fa79c3d809d7
Tugrul Topuz <tugrultopuz@gmail.com>
2025-04-29 17:02:06 +0300

    python:3.10-slim docker build

--- .github/actions/jetson/entrypoint.sh
+++ .github/actions/jetson/entrypoint.sh
@@ -5,17 +5,8 @@ git config --global --add safe.directory /github/workspace
 
 git submodule update --init --recursive
 
-./build.sh \
-    --allow_running_as_root \
-    --config Release \
-    --skip_tests \
-    --skip_onnx_tests \
-    --build_wheel \
-    --use_cuda \
-    --cuda_home=/usr/local/cuda-10.2 \
-    --cudnn_home=/usr/lib/aarch64-linux-gnu/ \
-    --parallel \
-    --update --build --build_wheel && \
-mkdir -p /github/workspace/build-files && \
-cp build/Linux/Release/dist/*.whl /github/workspace/build-files/
-
+V=1 VERBOSE=1 ./build.sh --allow_running_as_root --config Release --update --parallel --build --build_wheel \
+    --skip_tests  --skip_onnx_tests --use_cuda --cuda_home=/usr/local/cuda --tensorrt_home /usr/lib/aarch64-linux-gnu \
+    --cudnn_home=/usr/lib/aarch64-linux-gnu/ --cmake_extra_defines 'CMAKE_CUDA_ARCHITECTURES=53' \
+    'onnxruntime_BUILD_UNIT_TESTS=OFF' 'onnxruntime_USE_FLASH_ATTENTION=OFF' 'onnxruntime_USE_MEMORY_EFFICIENT_ATTENTION=OFF' && \
+mkdir -p /github/workspace/build-files && cp build/Linux/Release/dist/*.whl /github/workspace/build-files/
